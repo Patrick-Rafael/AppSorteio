de34417cc66c3b124173fb14a17a78765d14a5e9
Patrick <patrickrafael05@gmail.com>
2021-07-14 14:55:37 -0300

    Meu primeiro projeto

---
+++ app/src/main/java/com/example/sorteio/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sortear(View view){
+
+        TextView numero = findViewById(R.id.textoNumero);
+
+        int x = new Random().nextInt(11);
+
+       numero.setText( "Número: " + x );
+
+    }
+}
